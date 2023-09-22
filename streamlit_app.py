d49f38286805de8b1e507d6a2d2db5be9baa99aa
sithankanna <sithankanna@gmail.com>
2023-09-22 22:04:49 +0100

    Add chat app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,24 @@ import streamlit as st
 
 
 def main():
-    st.title("Hello, Streamlit!")
-    st.write("This is a simple Streamlit app.")
+    st.title("Simple chat")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
 
 if __name__ == "__main__":
